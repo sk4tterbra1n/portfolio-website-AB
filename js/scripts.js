08fb26a9e96a394a1ad6875192b9dfe25b1e77b8
sk4tterbra1n <abalmain18@gmail.com>
2022-03-26 11:34:06 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log("Hey! Welcome to my portfolio site");
+
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  }
+  else {
+    x.className = 'navtoggle';
+  }
+}
